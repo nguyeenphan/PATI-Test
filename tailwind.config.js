9963a29c83e4684fc437dcf77f8d7f8f32f85c08
nguyeenphan <phannguyenbh0902@gmail.com>
2026-01-19 10:30:27 +0700

    fix(tailwind): remove CSS from content paths, only scan HTML and JS

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
     content: [
-        "./*.html",
-        "./js/**/*.js",
-        "./css/**/*.css"
+        "./**/*.html",
+        "./js/**/*.js"
     ],
     theme: {
         extend: {
