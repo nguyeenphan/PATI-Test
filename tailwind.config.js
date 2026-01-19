72ede8a39998d56379cf628654201727656bd4e2
nguyeenphan <phannguyenbh0902@gmail.com>
2026-01-19 10:32:28 +0700

    huhu

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
     content: [
-        "./**/*.html",
-        "./js/**/*.js"
+        "./.html",
+        "./src/**/.{html,js}",
+        "./*.js"
     ],
     theme: {
         extend: {
